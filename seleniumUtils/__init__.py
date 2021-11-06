5eb9ba3906f9ffc4fcdac1d538b5733d257d7633
fiallo13ve <aliarjmandi339@gmail.com>
2021-11-06 06:04:32 +0330

    add some functions for selenium

--- seleniumUtils/__init__.py
+++ seleniumUtils/__init__.py
@@ -10,8 +10,10 @@ from selenium.webdriver.chrome.options import Options
 def init_drive(headless=None):
     current_directory_path = pathlib.Path(__file__).parent.absolute()
     chrome_driver_path = os.path.join(
-        current_directory_path, "./drivers/chromedriver")
+        current_directory_path, "./drivers/win95")
     options = Options()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
     if headless:
         options.add_argument("--headless")
     return webdriver.Chrome(options=options, executable_path=chrome_driver_path)
@@ -34,3 +36,36 @@ def wait_by_class(driver, element_class):
             time.sleep(1)
         except Exception as e:
             time.sleep(1)
+
+def wait_until_load(driver):
+    while True:
+        try:
+            page_state = driver.execute_script('return document.readyState;')
+            if page_state == 'complete': break
+            time.sleep(1)
+        except:
+            time.sleep(1)
+
+def find_by_class_eq(driver, element_class, multi=False):
+    elements =  driver.find_elements_by_xpath(".//*[@class='"+ element_class +"']")
+    if not len(elements): return
+    if multi: return elements
+    else: return elements[0]
+
+def find_by_class_includes(driver, element_class, multi=False):
+    elements =  driver.find_elements_by_xpath(".//*[contains(@class,'"+element_class+"')]")
+    if not len(elements): return
+    if multi: return elements
+    else: return elements[0]
+
+def find_by_tag(driver, tag, multi=False):
+    elements =  driver.find_elements_by_xpath(".//"+tag)
+    if not len(elements): return
+    if multi: return elements
+    else: return elements[0]
+
+def find_by_xpath(driver, xpath, multi=False):
+    elements =  driver.find_elements_by_xpath(xpath)
+    if not len(elements): return
+    if multi: return elements
+    else: return elements[0]
