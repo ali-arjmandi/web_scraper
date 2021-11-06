d4ed11fc7e3c1e871ad5908edb495e98379af029
fiallo13ve <aliarjmandi339@gmail.com>
2021-11-06 06:03:48 +0330

    editing translates

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def orderParser(order):
     lines = order.split("\n")
     arr = []
     for line in lines:
+        if line == "": break
         contents = line.split(":")
         if line[0] != ' ':
             arr.append(contents[0]+':')
-            if len(contents[1]) != 0:
-                arr[-1] += contents[1]
+            if len(contents[1:]) != 0:
+                arr[-1] += ':'.join(contents[1:])
         else:
             arr[-1] += '\n' + line
     for i in range(len(arr)):
@@ -19,17 +20,51 @@ def orderParser(order):
         arr[i] = {key: value}
     return arr
 
-def translater(order):
+def translater(order, variable="driver"):
     code = ""
     for itm in order:
         for k, v in itm.items():
             if k == "open":
-                code += f"print('please open {v}')\n"
+                code += f"base_url = '{v}'\ndriver.get(base_url)\n"
             elif k == "print":
-                code += f"print('{v}')\n"
+                d = {
+                    "attribute": None,
+                    "path": None,
+                }
+                for i in v:
+                    if list(i.keys())[0] == "attribute": d['attribute'] = list(i.values())[0]
+                    if list(i.keys())[0] == "path": d['path'] = list(i.values())[0]
+                if not d['attribute']: code += f"print(seleniumUtils.find_by_xpath({variable}, '{d['path']}').text)\n"
+                else: code += f"print(seleniumUtils.find_by_xpath({variable}, '{d['path']}').get_attribute('{d['attribute']}'))\n"
             elif k == "loop":
                 code += "while True:\n"
-                code += "\n".join(list(map(lambda line: "\t"+line, translater(v).split('\n'))))+"\n"
+                code += "\n".join(list(map(lambda line: "\t"+line, translater(v).split('\n'))))
+            elif k == "loopOver":
+                d = {
+                    "over": None,
+                    "value": None,
+                    "type": None,
+                    "then": None,
+                    "hasNewObject": None,
+                }
+                for i in v:
+                    if list(i.keys())[0] == "over": d['over'] = list(i.values())[0]
+                    if list(i.keys())[0] == "value": d['value'] = list(i.values())[0]
+                    elif list(i.keys())[0] == "type": d['type'] = list(i.values())[0]
+                    elif list(i.keys())[0] == "then": d['then'] = list(i.values())[0]
+                    elif list(i.keys())[0] == "hasNewObject": d['hasNewObject'] = list(i.values())[0]
+                code += f"element = seleniumUtils.find_by_xpath({variable}, '{d['over']}')\n"
+                code += "if not element: break\n"
+                code += f"elements = seleniumUtils.find_by_{d['type']}({variable}, '{d['value']}', multi=True)\n"
+                code += "if not elements: break\n"
+                code += "for element in elements:\n"
+                if d['then']:
+                    if d['hasNewObject']: code += "\tobj = {}\n"
+                    code += "\n".join(list(map(lambda line: "\t"+line, translater(d['then'], 'element').split('\n'))))
+                    if d['hasNewObject']:
+                        code += "objs.append(obj)\n"
+                        code += "\tprint(obj)\n"
+                else: code += "\tpass\n"
             elif k == "click":
                 d = {
                     "value": None,
@@ -40,14 +75,91 @@ def translater(order):
                     if list(i.keys())[0] == "value": d['value'] = list(i.values())[0]
                     elif list(i.keys())[0] == "type": d['type'] = list(i.values())[0]
                     elif list(i.keys())[0] == "then": d['then'] = list(i.values())[0]
-                code += f"print('clicking on {d['value']} in {d['type']} sections...')\n"
+                code += f"element = seleniumUtils.find_by_{d['type']}({variable}, '{d['value']}')\n"
+                code += "if not element: break\n"
+                code += "element.click()\n"
                 if d['then']:
                     code += translater(d['then'])
-
+            elif k == "clickNewTab":
+                d = {
+                    "value": None,
+                    "type": None,
+                    "then": None
+                }
+                for i in v:
+                    if list(i.keys())[0] == "value": d['value'] = list(i.values())[0]
+                    elif list(i.keys())[0] == "type": d['type'] = list(i.values())[0]
+                    elif list(i.keys())[0] == "then": d['then'] = list(i.values())[0]
+                code += f"element = seleniumUtils.find_by_{d['type']}({variable}, '{d['value']}')\n"
+                code += "if not element: break\n"
+                code += "element.send_keys(Keys.CONTROL + Keys.SHIFT + Keys.RETURN)\n"
+                code += "driver.switch_to.window(driver.window_handles[-1])\n"
+                code += "seleniumUtils.wait_until_load(driver)\n"
+                if d['then']:
+                    code += translater(d['then'])+"\n"
+                code += "driver.close()\n"
+                code += "driver.switch_to.window(driver.window_handles[-1])\n"
+            elif k == "addToObject":
+                code += "key = None\n"
+                code += "val = None\n"
+                code += translater(v, variable)
+                code += "if key and val: obj[key] = val\n"
+            elif k == "key":
+                d = {
+                    "attribute": None,
+                    "path": None,
+                    "slice": None
+                }
+                try:
+                    for i in v:
+                        if list(i.keys())[0] == "attribute": d['attribute'] = list(i.values())[0]
+                        if list(i.keys())[0] == "slice": d['slice'] = list(i.values())[0]
+                        if list(i.keys())[0] == "path": d['path'] = list(i.values())[0]
+                    if not d['attribute']: code += f"key = seleniumUtils.find_by_xpath({variable}, '{d['path']}').text\n"
+                    else: code += f"key = seleniumUtils.find_by_xpath({variable}, '{d['path']}').get_attribute('{d['attribute']}')\n"
+                    if d['slice']: code += f"key = key{d['slice']}\n"
+                except:
+                    code += f"key = '{v}'\n"
+            elif k == "val":
+                d = {
+                    "attribute": None,
+                    "path": None,
+                    "slice": None,
+                    "array": None,
+                    "value": None,
+                    "type": None,
+                }
+                try:
+                    for i in v:
+                        if list(i.keys())[0] == "attribute": d['attribute'] = list(i.values())[0]
+                        if list(i.keys())[0] == "slice": d['slice'] = list(i.values())[0]
+                        if list(i.keys())[0] == "path": d['path'] = list(i.values())[0]
+                        if list(i.keys())[0] == "array": d['array'] = list(i.values())[0]
+                        if list(i.keys())[0] == "value": d['value'] = list(i.values())[0]
+                        if list(i.keys())[0] == "type": d['type'] = list(i.values())[0]
+                    if not d['array']:
+                        if not d['attribute']: code += f"val = seleniumUtils.find_by_xpath({variable}, '{d['path']}').text\n"
+                        elif d['attribute'] and d['path']: code += f"val = seleniumUtils.find_by_xpath({variable}, '{d['path']}').get_attribute('{d['attribute']}')\n"
+                        if d['slice']: code += f"val = val{d['slice']}\n"
+                    else:
+                        code += "val = []\n"
+                        code += f"element = seleniumUtils.find_by_xpath({variable}, '{d['path']}')\n"
+                        code += "if not element: break\n"
+                        code += f"elements = seleniumUtils.find_by_{d['type']}(element, '{d['value']}', multi=True)\n"
+                        code += "if not elements: break\n"
+                        code += "for element in elements:\n"
+                        if not d['attribute']: code += f"\ttmpVal = element.text\n"
+                        elif d['attribute']: code += f"\ttmpVal = element.get_attribute('{d['attribute']}')\n"
+                        code += "\tval.append(tmpVal)\n"
+                except:
+                    code += f"val = '{v}'\n"
+                
     return code
 
 with open('order.arj') as f:
     order = f.read()
+with open('base.txt') as f:
+    base = f.read()
 
 
 
@@ -56,7 +168,8 @@ res = ""
 
 order = orderParser(order)
 res = translater(order)
+output = base.replace("@@code@@", res)
+
 with open('scraper.py', 'w') as f:
-    f.write(res)
-# import scraper
-import seleniumUtils
+    f.write(output)
+import scraper
